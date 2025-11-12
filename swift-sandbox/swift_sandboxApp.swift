ecd5da7386122a7b1dfadda1c24936ed83a92937
0xsj <sjtommylee@gmail.com>
2025-11-12 19:58:55 +0300

    Initial Commit

---
+++ swift-sandbox/swift_sandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_sandboxApp.swift
+//  swift-sandbox
+//
+//  Created by seung joon lee on 12.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct swift_sandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
